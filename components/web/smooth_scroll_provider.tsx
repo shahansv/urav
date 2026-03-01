dc579170b61268f6195ab85c84d73a2c3f0e5f6e
shahansv <shahanvsaleem@gmail.com>
2026-03-01 22:17:13 +0530

    lenis added

---
+++ components/web/smooth_scroll_provider.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import { ReactNode, useEffect, useRef } from "react";
+import Lenis from "lenis";
+
+export default function SmoothScrollProvider({
+  children,
+}: {
+  children: ReactNode;
+}) {
+  const lenisRef = useRef<Lenis | null>(null);
+
+  useEffect(() => {
+    const lenis = new Lenis({
+      duration: 1.2,
+      easing: (t: number) => 1 - Math.pow(1 - t, 4),
+      smoothWheel: true,
+    });
+
+    lenisRef.current = lenis;
+
+    function raf(time: number) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+
+    return () => {
+      lenis.destroy();
+      lenisRef.current = null;
+    };
+  }, []);
+
+  return <>{children}</>;
+}
